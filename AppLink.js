d488c67fa9356754bc5b54973078751f0112c988
Jo Lyshoel <jlysh@equinor.com>
2020-03-06 13:29:49 +0100

    Init upload

---
+++ AppLink.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function AppLink() {
+  return (
+    <a
+      className="App-link"
+      href="https://github.com/jlyshoel/react-example-app"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      Learn more about submodules
+    </a>
+  );
+}
+
+export default AppLink;
---
+++ AppText.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function AppLink() {
+  return (
+    <p>This text is loaded form the <code>src/shared/AppText.js</code> submodule.</p>
+  );
+}
+
+export default AppLink;
